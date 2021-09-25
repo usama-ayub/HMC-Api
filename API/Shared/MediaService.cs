1c68184c9da1d37d0b490b1f160455b6b2bd96c3
Ayub <usama.ayub@corp>
2021-09-25 21:57:30 +0500

    media service add

--- API/Shared/MediaService.cs
+++ API/Shared/MediaService.cs
@@ -9,33 +9,48 @@ namespace API.Shared
     public class MediaService : IMediaService
     {
         private readonly Cloudinary _cloudinary;
-        public MediaService(IOptions<CloudinarySetting> config){
+        public MediaService(IOptions<CloudinarySetting> config)
+        {
             var acc = new Account(
                 config.Value.CloudName,
                 config.Value.APIKey,
                 config.Value.APISecret
             );
-         _cloudinary = new Cloudinary(acc);
+            _cloudinary = new Cloudinary(acc);
         }
         public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
         {
             var imageUploadResult = new ImageUploadResult();
+
+            using var stream = file.OpenReadStream();
+            var imageParam = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
+            };
+            imageUploadResult = await _cloudinary.UploadAsync(imageParam);
+            
+            return imageUploadResult;
+        }
+
+        public async Task<ImageUploadResult> AddBase64PhotoAsync(string base64, string filename)
+        {
+            var imageUploadResult = new ImageUploadResult();
             // if (file.Length != 0)
             // {
-                using var stream = file.OpenReadStream();
-                var imageParam = new ImageUploadParams
-                {
-                    File = new FileDescription(file.FileName, stream),
-                    Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
-                };
-                imageUploadResult = await _cloudinary.UploadAsync(imageParam);
-                return imageUploadResult;
+            var imageParam = new ImageUploadParams
+            {
+                File = new FileDescription(filename, $"@{base64}"),
+                Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
+            };
+            imageUploadResult = await _cloudinary.UploadAsync(imageParam);
+            return imageUploadResult;
             // }
         }
 
-        public Task<DeletionResult> DeletePhotoAsync(string PublicId)
+        public Task<DeletionResult> DeletePhotoAsync(string publicId)
         {
-            var deleteParams = new DeletionParams(PublicId);
+            var deleteParams = new DeletionParams(publicId);
             var result = _cloudinary.DestroyAsync(deleteParams);
             return result;
         }
@@ -44,7 +59,8 @@ namespace API.Shared
     public interface IMediaService
     {
         Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
- Task<DeletionResult> DeletePhotoAsync(string PublicId);
+        Task<ImageUploadResult> AddBase64PhotoAsync(string base64, string filename);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
 
     }
 }
